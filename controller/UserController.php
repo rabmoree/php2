febf816f4fd9fbc0c627a6d4a7f55142e1721ea2
Rabee N. Moraga <rabmoree@gmail.com>
2019-08-03 13:26:19 +0800

    Activity 3

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,35 +1,85 @@
 <?php
 require("../model/UserModel.php");
+$data = new UserModel();
 class UserController{
     private $userModel;
 
     public function __construct(){
         $this->userModel = new UserModel();
     }
+
     public function addUser($name, $email,$username, $password){
         //perform data validation
-        $this->userModel->name = $name;
-        $this->userModel->email = $email;
-        $this->userModel->username = $username;
-        $this->userModel->password = $password;
-        $this->userModel->insert();
+        if(empty($name) || empty($email) || empty($username) || empty($password)) {
+            return "Please fill up required fields";
+        }
+        else {
+            $this->userModel->name = $name;
+            $this->userModel->email = $email;
+            $this->userModel->username = $username;
+            $this->userModel->password = $password;
+            $this->userModel->insert();
+            return true;
+        }
+    }
+
+    public function updateUser($id, $password2){
+        $test = $this->userModel->userID = $id;
+        $res = $this->userModel->readData2();
+        $rows = mysqli_num_rows($res);
+
+        if(empty($id) || empty($password2)){
+            return "Connot update with empty value";
+        }
+        else if($rows == 0 ) {
+            return "Connot update! User does not exist";
+        }
+        else {
+            $this->userModel->userID = $id;
+            $this->userModel->password2 = $password2;
+            $this->userModel->updateData();
+            return true;
+        }
     }
 
     public function getAllUser(){
-        return $this->userModel->readAll();
+        return $this->userModel->readData();
+    }
+
+    public function deleteUser($id){
+        $test = $this->userModel->userID = $id;
+        $res = $this->userModel->readData2();
+        $rows = mysqli_num_rows($res);
+
+        if(empty($id)){
+            return "Connot update with empty value";
+        }
+        else if($rows == 0 ) {
+            return "Connot update! User does not exist";
+        }
+        else {
+            $this->userModel->userID = $id;
+            $this->userModel->deleteData();
+            return true;
+        }
+
+        // global $data;
+        // $res = $data->readData();
+        // while($info = mysqli_fetch_assoc($res)){
+        //     if($info['id'] != $id){
+        //         return "Connot delete! User does not exist!";
+        //     }
+        //     else {
+        //         $this->userModel->userID = $id;
+        //         $this->userModel->deleteData();
+        //         return true;
+        //     }
+
+        // }
     }
 
     public function __destruct(){
         unset($this->userModel);
     }
 }
-// $userCont = new UserController();
-// $return = $userCont->getAllUser();
-// while($data = mysqli_fetch_assoc($return)){
-//     echo "name: ".$data["name"]."<br/>";
-//     echo "email: ".$data["email"]."<br/>";
-//     echo "username: ".$data["username"]."<br/>";
-//     echo "password: ".$data["password"]."<br/>";
-//     echo "------------------------------------------<br/>";
-// }
 ?>
--- controller/unit_test.php
+++ controller/unit_test.php
@@ -1,15 +1,96 @@
 <?php
-require ("UserController.php");
+require("UserController.php");
 
-$data = new UserController();
+$userCont = new UserController();
 
-echo $data->addUser();
-// if($res) {
-//     return true;
-// }
-// else {
-//     return "error";
-// }
+    insert();
+    update();
+    readAll();
+    delete();
+    insertFalse();
+    updateFalse1();
+    updateFalse2();
+    deleteFalse();
 
+    function insert(){
+        global $userCont;
+        $name = "daingnainday";
+        $email = "dindin@g.com";
+        $username = "dindin";
+        $password = "12345000";
+        $result = $userCont->addUser($name,$email,$username,$password);
+        echo "Test Case #1 Inserting record: ";
+        var_dump($result);
+        echo "<br>";
+    }
 
+    function update(){
+        global $userCont;
+        $id = "7";
+        $password2 = "okieies";
+        $result = $userCont->updateUser($id, $password2);
+        echo "Test Case #2 Updating record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function readAll(){
+        global $userCont;
+        $result = $userCont->getAllUser();
+        echo "Test Case #3 Read All record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function delete(){
+        global $userCont;
+        $id = "15";
+        $result = $userCont->deleteUser($id);
+        echo "Test Case #4 Deleting record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function insertFalse(){
+        global $userCont;
+        $name = "";
+        $email = "dindin@g.com";
+        $username = "dindin";
+        $password = "12345000";
+        $result = $userCont->addUser($name,$email,$username,$password);
+        echo "Test Case #5 Inserting record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function updateFalse1(){
+        global $userCont;
+        $id = "4";
+        $password2 = "";
+        $result = $userCont->updateUser($id, $password2);
+        echo "Test Case #6 Updating record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function updateFalse2(){
+        global $userCont;
+        $id = "66";
+        $password2 = "whatsup";
+        $result = $userCont->updateUser($id, $password2);
+        echo "Test Case #7 Updating record: ";
+        var_dump($result);
+        echo "<br>";
+    }
+
+    function deleteFalse(){
+        global $userCont;
+        $id = "2";
+        $result = $userCont->deleteUser($id);
+        echo "Test Case #8 Deleting record:";
+        var_dump($result);
+        echo "<br>";
+        // echo "hello?";
+    }
+    
 ?>
